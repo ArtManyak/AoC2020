715ace85ac94d980e9f5a39cd3e35665dc809f49
artmanyak <artmanyak@gmail.com>
2020-12-02 11:13:29 +0500

    day 2

--- common/common.py
+++ common/common.py
@@ -4,3 +4,8 @@ from typing import List
 def getNumbers(file_path: str) -> List[int]:
     with open(file_path) as file:
         return list(map(int, file.readlines()))
+
+
+def getLines(file_path: str) -> List[str]:
+    with open(file_path) as file:
+        return file.readlines()
---
+++ day02/day02.py
@@ -0,0 +1,33 @@
+from common.common import getLines
+
+
+def parse_line(line: str) -> (int, int, str, str):
+    policy, password = line.split(':')
+    min_max, letter = policy.split(' ')
+    lower_bound, upper_bound = map(int, min_max.split('-'))
+    return lower_bound, upper_bound, letter, password
+
+
+def solve() -> int:
+    lines = getLines('in.txt')
+    return len(list(filter(is_valid_pass, lines)))
+
+
+def is_valid_pass(line: str) -> bool:
+    lower_bound, upper_bound, letter, password = parse_line(line)
+    return lower_bound <= password.count(letter) <= upper_bound
+
+
+def solve2() -> int:
+    lines = getLines('in.txt')
+    return len(list(filter(is_valid_pass2, lines)))
+
+
+def is_valid_pass2(line: str) -> bool:
+    lower_bound, upper_bound, letter, password = parse_line(line)
+    return (password[lower_bound] == letter) ^ (password[upper_bound] == letter)
+
+
+if __name__ == '__main__':
+    print(solve())
+    print(solve2())
